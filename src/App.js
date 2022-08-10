50c353f027b4968a62a9fb471add7065976f91dd
DesarrolloColectivo <zasalastobon@gmail.com>
2022-08-10 13:45:04 -0500

    add request data axios and apply dark style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { TableBasic } from "./tables/TableBasic";
 import { TableJson } from "./tables/TableJson";
+import { TableAxios } from "./tables/TableAxios";
+import { Content } from "./layout/content/content";
 
 function App() {
   return (
     <div className="App">
+      {/* use data array */}
       <TableBasic />
+      {/*  use data file json */}
       <TableJson />
+      {/* apply dark style */}
+      <Content>
+        <TableAxios />
+      </Content>
     </div>
   );
 }
--- src/tables/TableAxios.js
+++ src/tables/TableAxios.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MUIDataTable from "mui-datatables";
 import users from "./users.json";
+import axios from "axios";
 const columns = [
   { name: "id", label: "ID" },
-  { name: "name", label: "NOMBRE" },
-  { name: "email", label: "CORREO" },
+  { name: "title", label: "Nombre" },
+  { name: "category", label: "Categoria" },
+  { name: "price", label: "Precio" },
 ];
 
 function TableAxios() {
   //define and config hooks
-
-  //funtion
+  const [products, setProducts] = useState([]);
+  //define variable data
+  const endpoint = "https://fakestoreapi.com/products";
+  //define function with axios
+  const getData = async () => {
+    await axios.get(endpoint).then((response) => {
+      const data = response.data;
+      console.log(data);
+      setProducts(data);
+    });
+  };
+  //call function getData with hooks useEffect
+  useEffect(() => {
+    getData();
+  }, []);
 
   //render data
   return (
-    <MUIDataTable title={"Lista de Empleados"} data={users} columns={columns} />
+    <MUIDataTable
+      title={"Lista de Empleados"}
+      data={products}
+      columns={columns}
+    />
   );
 }
 
